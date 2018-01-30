0f73e1194e738dd4e082ca083722c9683da4ee3f
mladen <diskozub@hotmail.com>
2018-01-30 15:57:13 +0100

    connecting with username

--- server/src/index.js
+++ server/src/index.js
@@ -1,36 +1,53 @@
 var ws = require('ws')
+var url = require('url')
 
 const port = 3001
 
 const wss = new ws.Server({
-	port: port 
+	port: port ,
+	verifyClient: (info,cb) =>{
+		//extract username from url
+		let username = url.parse(info.req.url,true).query.username
+		if(!username) cb(false,401,"No username provided")
+		info.req.id=username
+		return cb(true)
+	}
 })
 
-var users={}
-
+var USERS={}
+var count=0
 wss.on('connection',socket =>{
-	let id = count++
-	socket.id=id
-	users[id] = socket.id
-	console.log(`user connected id=${id}`)
-	console.log("allusers : "+Object.keys(users))
 
+	let id = socket.upgradeReq.id
+	//check if username is available
+	if(Object.keys(USERS).includes(id)){
+		socket.send(`SERVER >> Username '${id}' not available`)
+		id=id+count++
+		socket.send(`SERVER >> Your username is set to '${id}'`)
+	}
+	socket.id=id
+	USERS[id] = socket.id
+
+	console.log(`user connected id=${id}`)
+	console.log("allusers : "+Object.keys(USERS))
+
+	socket.send(`SERVER >> hi , ${id}`)
+
+	//EVENTS
 	socket.on('message', message =>{
 		console.log(`new message ${socket.id}: ${message}`)
 		wss.broadcast(message)
 	})
 
 	socket.on('close',_=>{
-		delete users[socket.id]
+		delete USERS[socket.id]
 		console.log(`user disconnected id=${socket.id}`)
-		console.log("allusers : "+Object.keys(users))
+		console.log("allusers : "+Object.keys(USERS))
 	})
 
 	socket.on('error',e=>{
 		console.log("socketerror : " +e)
-	})
-
-	socket.send('hi');	
+	})	
 })
 
 wss.on('error',e=>{
