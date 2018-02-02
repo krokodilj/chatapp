0825c1ec0d45b1bd6b0cda9769949d3eeb64e7d3
mladen <diskozub@hotmail.com>
2018-02-02 16:48:33 +0100

    conflict if username in use

--- server/src/api/user.api.js
+++ server/src/api/user.api.js
@@ -5,11 +5,18 @@ const userCtrl = require('../controllers/user.ctrl')
 
 router.post('/',(req,res)=>{
     let user = req.body
-    userCtrl.save(user,(err,results)=>{
+    userCtrl.getOneByUsername(user.username,(err,result)=>{
         if(err) res.status(500).send()
-        else res.status(200).json(results).send()
+        else if(result){
+            res.status(409).send()
+        }else{
+            userCtrl.save(user,(err,results)=>{
+                if(err) res.status(500).send()
+                else res.status(200).json(results).send()
+            })   
+        }
     })
-    
+        
 })
 
 router.get('/:id',(req,res)=>{
@@ -28,18 +35,17 @@ router.post('/login',(req,res)=>{
     let loginParams = req.body
     userCtrl.getOneByUsername(loginParams.username,(err,user)=>{
         if(err) res.status(500).send()
-        else{
-            if(!user){
-                res.status(404).send()
-            }else if (user.password!=loginParams.password){
-                res.status(401).send()
-            }else{
-                //ovde je sve dobro
-                //TODO zavrsi token!
-                token="asdasdasd.asdasd.asdasdasd"
-                res.status(200).json(token).send
-            }
+        else if(!user){
+            res.status(404).send()
+        }else if (user.password!=loginParams.password){
+            res.status(401).send()
+        }else{
+            //ovde je sve dobro
+            //TODO zavrsi token!
+            token="asdasdasd.asdasd.asdasdasd"
+            res.status(200).json(token).send
         }
+        
     })
 })
 
